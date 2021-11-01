f657f443da715c9e9ed86162256d538099b3bc56
joe-liad <92937667+joe-liad@users.noreply.github.com>
2021-11-01 08:23:14 +0000

    shell script to make ward pages

---
+++ create-md-pages.sh
@@ -0,0 +1,209 @@
+cat > "Bellingham.md" << EOF
+---
+title: Bellingham
+date: Last Modified
+eleventyNavigation:
+  order: 10
+  parent: home
+  key: Bellingham
+  title: Bellingham
+---
+EOF
+cat > "Blackheath.md" << EOF
+---
+title: Blackheath
+date: Last Modified
+eleventyNavigation:
+  order: 20
+  parent: home
+  key: Blackheath
+  title: Blackheath
+---
+EOF
+cat > "Brockley.md" << EOF
+---
+title: Brockley
+date: Last Modified
+eleventyNavigation:
+  order: 30
+  parent: home
+  key: Brockley
+  title: Brockley
+---
+EOF
+cat > "Catford South.md" << EOF
+---
+title: Catford South
+date: Last Modified
+eleventyNavigation:
+  order: 40
+  parent: home
+  key: Catford South
+  title: Catford South
+---
+EOF
+cat > "Crofton Park.md" << EOF
+---
+title: Crofton Park
+date: Last Modified
+eleventyNavigation:
+  order: 50
+  parent: home
+  key: Crofton Park
+  title: Crofton Park
+---
+EOF
+cat > "Deptford.md" << EOF
+---
+title: Deptford
+date: Last Modified
+eleventyNavigation:
+  order: 60
+  parent: home
+  key: Deptford
+  title: Deptford
+---
+EOF
+cat > "Downham.md" << EOF
+---
+title: Downham
+date: Last Modified
+eleventyNavigation:
+  order: 70
+  parent: home
+  key: Downham
+  title: Downham
+---
+EOF
+cat > "Evelyn.md" << EOF
+---
+title: Evelyn
+date: Last Modified
+eleventyNavigation:
+  order: 80
+  parent: home
+  key: Evelyn
+  title: Evelyn
+---
+EOF
+cat > "Forest Hill.md" << EOF
+---
+title: Forest Hill
+date: Last Modified
+eleventyNavigation:
+  order: 90
+  parent: home
+  key: Forest Hill
+  title: Forest Hill
+---
+EOF
+cat > "Grove Park.md" << EOF
+---
+title: Grove Park
+date: Last Modified
+eleventyNavigation:
+  order: 100
+  parent: home
+  key: Grove Park
+  title: Grove Park
+---
+EOF
+cat > "Hither Green.md" << EOF
+---
+title: Hither Green
+date: Last Modified
+eleventyNavigation:
+  order: 110
+  parent: home
+  key: Hither Green
+  title: Hither Green
+---
+EOF
+cat > "Ladywell.md" << EOF
+---
+title: Ladywell
+date: Last Modified
+eleventyNavigation:
+  order: 120
+  parent: home
+  key: Ladywell
+  title: Ladywell
+---
+EOF
+cat > "Lee Green.md" << EOF
+---
+title: Lee Green
+date: Last Modified
+eleventyNavigation:
+  order: 130
+  parent: home
+  key: Lee Green
+  title: Lee Green
+---
+EOF
+cat > "Lewisham Central.md" << EOF
+---
+title: Lewisham Central
+date: Last Modified
+eleventyNavigation:
+  order: 140
+  parent: home
+  key: Lewisham Central
+  title: Lewisham Central
+---
+EOF
+cat > "New Cross Gate.md" << EOF
+---
+title: New Cross Gate
+date: Last Modified
+eleventyNavigation:
+  order: 150
+  parent: home
+  key: New Cross Gate
+  title: New Cross Gate
+---
+EOF
+cat > "Perry Vale.md" << EOF
+---
+title: Perry Vale
+date: Last Modified
+eleventyNavigation:
+  order: 160
+  parent: home
+  key: Perry Vale
+  title: Perry Vale
+---
+EOF
+cat > "Rushey Green.md" << EOF
+---
+title: Rushey Green
+date: Last Modified
+eleventyNavigation:
+  order: 170
+  parent: home
+  key: Rushey Green
+  title: Rushey Green
+---
+EOF
+cat > "Sydenham.md" << EOF
+---
+title: Sydenham
+date: Last Modified
+eleventyNavigation:
+  order: 180
+  parent: home
+  key: Sydenham
+  title: Sydenham
+---
+EOF
+cat > "Telegraph Hill.md" << EOF
+---
+title: Telegraph Hill
+date: Last Modified
+eleventyNavigation:
+  order: 190
+  parent: home
+  key: Telegraph Hill
+  title: Telegraph Hill
+---
+EOF
